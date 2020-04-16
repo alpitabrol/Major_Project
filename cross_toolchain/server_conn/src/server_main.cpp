15b2ff8060c1b16e0861536c2386b43375d4d42e
Alpit Abrol <apt.abl@gmail.com>
2020-04-15 22:57:36 -0400

    coded added to print result on html page (needs to find out a bit of issues)

--- cross_toolchain/server_conn/src/server_main.cpp
+++ cross_toolchain/server_conn/src/server_main.cpp
@@ -2,20 +2,20 @@
 #include "SocketServer.h"
 #include <string.h>
 #include <sstream>
-#include<fstream>
+#include <fstream>
 #include<sstream>
-
+#include "html_page_2.h"
 using namespace std;
 using namespace exploringRPi;
 
 int main(int argc, char *argv[]){
 
-/*
+
 SocketServer server(54321);
 server.listen();
 string msg = server.receive(1024);
-*/
-string msg = "4132#2#4186#3#4060#2#4030#2#4631#2#4380#2#3155#2#4052#1#";
+
+//string msg = "4132#2#4186#3#4060#2#4030#2#4631#2#4380#2#3155#2#4052#2#";
 
 //string msg = "4219#3#33933#3#84186#1#4049#3#";
 
@@ -60,7 +60,7 @@ int code_counter=0,weight_counter=0;
     		weight_counter++;
     	}
      }
-
+/*
 cout<<"Input Code:"<<endl;
 for(int i=0;i<code_counter;i++)
 {
@@ -73,7 +73,7 @@ for(int i=0;i<weight_counter;i++)
 cout<<user_input_weight[i];
 cout<<endl;
 }
-
+*/
 
 string data[306][6];
     ifstream file("/home/devil/Desktop/NUTRITIONAL-TRACKER/cross_toolchain/server_conn/Debug/data.csv");
@@ -130,10 +130,10 @@ for (int i = 1; i < 306; ++i)
 }
 
 
-string second_page_info[code_counter][6];
+string second_page_info[code_counter][7];
 
 //inserting values as per second page requirements
- for (int i = 0; i < code_counter; ++i)
+ /*for (int i = 0; i < code_counter; ++i)
  {
 	 second_page_info[i][0]=user_input_info[i][0]; //plu code
 	 second_page_info[i][1]=user_input_info[i][1]; //name
@@ -150,13 +150,13 @@ string second_page_info[code_counter][6];
  		cout<<second_page_info[i][j]<<" ";
  	}
  cout<<"\n";
- }
+ }*/
 
  //PLU_Code,	Name,	Variety,	Size,	Pieces_In_A_LB,	Portions_In_A_LB,	bought_weight
  // 0			1		2			3		4				5					6
  int portions_bought[code_counter];
  int pieces_bought;
-
+ int sum_of_protions =0;
  for (int i = 0; i < code_counter; ++i)
  {
 
@@ -172,8 +172,8 @@ string second_page_info[code_counter][6];
 	 portions_bought[i] = pieces_bought / stoi(user_input_info[i][5]);
 
 	 }
-
-	 cout<<portions_bought[i]<<endl;
+sum_of_protions += portions_bought[i];
+	// cout<<portions_bought[i]<<endl;
  }
 
  string portion_bought_string[code_counter];
@@ -185,25 +185,40 @@ string second_page_info[code_counter][6];
 
  for (int i = 0; i < code_counter; ++i)
  {
-	 second_page_info[i][0]=user_input_info[i][0]; //plu code
-	 second_page_info[i][1]=user_input_info[i][1]; //name
-	 second_page_info[i][2]=user_input_info[i][2]; //variety
-	 second_page_info[i][3]=user_input_info[i][3]; //size
-	 second_page_info[i][4]=user_input_info[i][6]; //weight bought
-	 second_page_info[i][5]=portion_bought_string[i]; //number of portions bought
-}
+	 second_page_info[i][0]=user_input_info[i][0];     //plu code
+	 second_page_info[i][1]=user_input_info[i][1];      //name
+	 second_page_info[i][2]=user_input_info[i][2];     //variety
+	 second_page_info[i][3]=user_input_info[i][3];     //size
+	 second_page_info[i][4]=user_input_info[i][6];    //weight bought
+	 second_page_info[i][6]=portion_bought_string[i]; //number of portions bought
 
- for (int i = 0; i < code_counter; ++i)
- {
- 	for (int j = 0; j < 6; ++j)
- 	{
- 		cout<<second_page_info[i][j]<<" ";
- 	}
- cout<<"\n";
+	 if(stoi(user_input_info[i][4])== 1)
+		{
+		 string temp= user_input_info[i][5];
+
+		temp= "1/" + temp;
+
+		 second_page_info[i][5] = temp;       //"1/"+user_input_info[i][5];
+
+		 temp="";
+		}
+	 else
+	 {
+		 second_page_info[i][5]=user_input_info[i][5];     //pieces in a portion
+	 }
+
+
+
+//	 second_page_info[i][5]=user_input_info[i][5];     //pieces in a portion
  }
+
+int days_to_go = sum_of_protions/5;
+int portions_to_go = sum_of_protions%5;
+
+
  //string analysed_string[5][5];
 
-
+html_page(code_counter,second_page_info,days_to_go,portions_to_go);
 
 
 return 0;
